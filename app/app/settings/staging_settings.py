3acbf89d9a1d0566e382d8a49798d7554581fc33
arjaynguyen <arjaynguyen123@hotmail.com>
2017-11-14 18:00:55 -0800

    separated dev, prod, and staging settings

---
+++ app/app/settings/staging_settings.py
@@ -0,0 +1,19 @@
+from base_settings import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["127.0.0.1", "10.0.2.15", "localhost"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'bundles/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack/dev-webpack-stats.json'),
+    }
+}
