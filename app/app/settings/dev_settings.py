55d4ec82390f51f8e100a6c885aac0add40136f7
arjaynguyen <arjaynguyen123@hotmail.com>
2017-11-14 19:09:15 -0800

    update dev settings

--- app/app/settings/dev_settings.py
+++ app/app/settings/dev_settings.py
@@ -8,3 +8,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'bundles/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack/dev-webpack-stats.json'),
+    }
+}
